36a8d9ba30a77602957464ed7553ef5d0a890fe6
Your Name <you@example.com>
2017-02-05 00:45:21 -0200

    Config Standalone.xml

--- configuration/wildfly/configura.sh
+++ configuration/wildfly/configura.sh
@@ -4,7 +4,13 @@ ARQUIVO=standalone.xml
 USERBANCO=$1
 SENHABANCO=$2
 URLBANCO=$3
+LOCALCONFIG=$4
+
 cp $ARQUIVO $ARQUIVO'_new'
 sed -ie 's/{{userbanco}}/'$1'/g' $ARQUIVO'_new'
 sed -ie 's/{{senhabanco}}/'$2'/g' $ARQUIVO'_new'
 sed -ie 's/{{urlbanco}}/'$3'/g' $ARQUIVO'_new'
+
+cp -f $ARQUIVO'_new' LOCALCONFIG'standalone.xml'
+
+#ex. configura.sh userbanco senhabanco jdbc:postgresql://ip:5432/database /opt/wildfly/standalone/configuration/
