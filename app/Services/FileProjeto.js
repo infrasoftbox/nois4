7b8044ac2e3cf056ade920c3cf42a677bf83cceb
Wanderson A. Pereira <wandersonpereira@softbox.com.br>
2017-02-05 03:06:10 -0200

    Correção de merge

--- app/Services/FileProjeto.js
+++ app/Services/FileProjeto.js
@@ -2,7 +2,18 @@ var exec = require('child_process').exec;
 
 function FileProjeto () {
     this.gerarZip = function(arquivo) {
-        
+        //Devolve o caminho da aplicação
+        exec("pwd", function (error, stdout, stderr) {
+
+            var diretorio = stdout.replace(/\n/g, '') + "";
+
+            exec("zip -r " + diretorio + "/Pacotes/" + depencia + ".zip " + diretorio + "/docker/", function (error, stdout, stderr) { 
+                exec("unzip -o " + diretorio + "/docker/" + depencia + ".zip -d " + diretorio + "/docker/", function (errorz, stdoutz, stderrz) {
+                    exec("rm -rf " + diretorio + "/docker/" + depencia + ".zip", function () {})
+                })
+            })
+
+        });
     }
 }
 
